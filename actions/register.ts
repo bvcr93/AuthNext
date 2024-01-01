45d29d4f7341ad4dece1e2b7da483db431d1bbf5
bvcr93 <darelbavcar1@gmail.com>
2024-01-01 13:07:01 +0100

    user creation, utils

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,33 @@
 "use server";
-
-import { LoginSchema, RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  const validatedFields = LoginSchema.safeParse(values);
+  const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
-  return { success: "Email sent!" };
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // send ver token email
+
+  return { success: "User created!" };
 };
